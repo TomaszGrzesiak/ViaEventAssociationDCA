7c135b2d5d96d20ff98dd8ce5987cc0b00486954
Tomasz Grzesiak <grzesiaktomasz@gmail.com>
2025-02-28 14:22:17 +0100

    fix(UnitTests): Settings update .net 8.0 -> .net 9.0. Example classes and test

---
+++ ViaEventAssociation.Core.Tools.OperationResult/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace ViaEventAssociation.Core.Tools.OperationResult;
+
+public class Calculator
+{
+    public int A { get; }
+    public int B { get; }
+
+    public Calculator(int a, int b)
+    {
+        A = a;
+        B = b;
+    }
+
+    public int Sum() => A + B;
+}
