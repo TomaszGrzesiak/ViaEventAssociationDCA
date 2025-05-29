4b2f3fd8810686fe40255b150a4352f943c98e6e
Tomasz Grzesiak <grzesiaktomasz@gmail.com>
2025-05-29 22:37:46 +0200

    update(Result.cs and other): Updated Result.cs so that in includes previously forgotten requirements from Assignment 3: - Multiple Errors - Error Codes

--- ViaEventAssociation.Core.Tools.OperationResult/Result.cs
+++ ViaEventAssociation.Core.Tools.OperationResult/Result.cs
@@ -4,40 +4,19 @@ using System.Linq;
 
 public class Result
 {
-    // used "init" instead of "set" to exclude potential mutability. At the moment of first creation of the class, "set" would do the work. But in the case of adding/changing something in the class later, "init" is safer.
-    public bool IsSuccess { get; protected init; }
+    // used "init" and "IReadOnlyList" instead of "set" and "List" to exclude potential mutability.
+    public IReadOnlyList<ResultError> Errors { get; protected init; } = new List<ResultError>();
+    public bool IsSuccess => Errors.Count == 0;
     public bool IsFailure => !IsSuccess;
+    
+    public static Result Success() => new Result();
 
-    public List<string> ErrorMessages { get; protected init; } = new List<string>();
-    //public String? ErrorMessage;
-    
-    
-    public static Result Success() => new Result() { IsSuccess = true };
-    
-    /**
-     * "params" is convenient, and it works that you can use as follows:
-     *
-     * Result.Failure("Error 1");
-     * Result.Failure("Error 1", "Error 2", "Error 3");
-     *
-     * and the arguments are automatically packed into a string[]
-     */
-    public static Result Failure(IEnumerable<string> errors)
+    // params allows to use Result.Failure(error1, error2, error3); instead of Result.Failure(new[] { error1, error2, error3 });
+    public static Result Failure(params ResultError[]  errors) => new Result() { Errors = errors.ToList() };
+    public static Result Combine(Result original, params ResultError[] errors)
     {
-        var errorList = errors switch
-        {
-            // if errors is a List<string>...
-            List<string> list => list,
-            // if it's just a simple array[]
-            string[] array => array.ToList(),
-            // _ means, that the switch can match anything, f.x. a "null" parameter, f.x. in the case where we forget to set an error message and use "null" instead
-            _ => errors.ToList()
-        };
-        return new Result()
-        {
-            IsSuccess = false,
-            ErrorMessages = errorList
-        };
+        var combined = original.Errors.Concat(errors).ToList();
+        return new Result { Errors = combined };
     }
 }
 
@@ -45,32 +24,26 @@ public class Result<T> : Result
 {
     public T? Payload { get; private init; }
     
-    
-    private Result(List<string>? errorMessages)
+    private Result(List<ResultError> errors)
     {
+        if (errors == null || errors.Count == 0)
+            throw new ArgumentException("Failure must contain at least one error.");
         Payload = default; // default sets the value to 0, null or other default value of the particular object
-        IsSuccess = false;
-        ErrorMessages = errorMessages ?? ["Unknown error. This message should never be displayed."];
+        Errors = errors;
     }
     
-    public static Result<T> Success(T value) => new Result<T>(null)
+    private Result(T value)
     {
-        Payload = value,
-        IsSuccess = true,
-        ErrorMessages = new List<string>()
-    };
+        Payload = value;
+        Errors = new List<ResultError>();
+    }
+
+    public static Result<T> Success(T value) => new Result<T>(value);
     
-    // "new" because this method has the same body as in super class, and thus it has to overwrite the superclass method. C# does this by default, but it likes to state it implicitly with the "new" keyword.
-    // Failure accepts only IEnumerable<string>, so minimum an "array". It shouldn't be all that big of a deal, because it only needs adding square brackets, f.x. ["error"] instead of "error".
-    public static Result<T> Failure(string errorMessage) =>
-        new Result<T>(new List<string> { errorMessage });
+    public static Result<T> Failure(ResultError error) =>
+        new Result<T>([error]);
 
-    public static Result<T> Failure(params string[] errorMessages) =>
-        new Result<T>(errorMessages.ToList());
-
-    public static Result<T> Failure(List<string> errorMessages) =>
-        new Result<T>(errorMessages);
+    public new static Result<T> Failure(params ResultError[] errors) =>
+        new Result<T>(errors.ToList());
 }
-
-
-public class None {}
+public sealed record None {}
