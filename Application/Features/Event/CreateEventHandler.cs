0463c23fa8841fa3d516beba4109652ae496e9ad
318212 <318212@via.dk>
2025-06-04 03:57:36 +0200

    createEvent handler implementation (it does require some more fixes later)

--- Application/Features/Event/CreateEventHandler.cs
+++ Application/Features/Event/CreateEventHandler.cs
@@ -1,5 +1,6 @@
 using Application.AppEntry;
 using Application.AppEntry.Commands.Event;
+using ViaEventAssociation.Core.Domain.Aggregates.Events;
 using ViaEventAssociation.Core.Domain.Common;
 using ViaEventAssociation.Core.Domain.Repositories;
 using ViaEventAssociation.Core.Tools.OperationResult;
@@ -10,10 +11,33 @@ public class CreateEventHandler : ICommandHandler<CreateEventCommand>
 {
     private readonly IEventRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
-    
-    
-    public Task<Result> Handle(CreateEventCommand command)
+
+    public CreateEventHandler(IEventRepository repository, IUnitOfWork unitOfWork)
     {
-        throw new NotImplementedException();
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result> Handle(CreateEventCommand command)
+    {
+        EventId eventId = new EventId(Guid.Empty);
+        var result = ViaEventAssociation.Core.Domain.Aggregates.Events.Event.CreateEvent();
+        // ViaEventAssociation.Core.Domain.Aggregates.Events.Event createdEvent = result.Payload;
+        ViaEventAssociation.Core.Domain.Aggregates.Events.Event createdEvent = (
+            (Result<ViaEventAssociation.Core.Domain.Aggregates.Events.Event>)result
+        ).Payload;
+
+        if (result.IsSuccess)
+        {
+            await _repository.AddAsync(createdEvent);
+            var GetEvent = await _repository.GetViaEventByIdAsync(command.Id);
+
+            await _unitOfWork.SaveChangesAsync();
+
+            return new Result();
+        }
+
+        return new Result();
+
     }
 }
