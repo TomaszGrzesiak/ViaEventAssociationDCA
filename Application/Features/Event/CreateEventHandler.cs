88e171f3d08cbbe114ff64b5ad2d86a9553f6da7
318212 <318212@via.dk>
2025-06-05 02:59:56 +0200

    Removed faulty implementation of Handle from CreateEventHandler.cs

--- Application/Features/Event/CreateEventHandler.cs
+++ Application/Features/Event/CreateEventHandler.cs
@@ -20,24 +20,7 @@ public class CreateEventHandler : ICommandHandler<CreateEventCommand>
 
     public async Task<Result> Handle(CreateEventCommand command)
     {
-        EventId eventId = new EventId(Guid.Empty);
-        var result = ViaEventAssociation.Core.Domain.Aggregates.Events.Event.CreateEvent();
-        // ViaEventAssociation.Core.Domain.Aggregates.Events.Event createdEvent = result.Payload;
-        ViaEventAssociation.Core.Domain.Aggregates.Events.Event createdEvent = (
-            (Result<ViaEventAssociation.Core.Domain.Aggregates.Events.Event>)result
-        ).Payload;
-
-        if (result.IsSuccess)
-        {
-            await _repository.AddAsync(createdEvent);
-            var GetEvent = await _repository.GetViaEventByIdAsync(command.Id);
-
-            await _unitOfWork.SaveChangesAsync();
-
-            return new Result();
-        }
-
-        return new Result();
-
+        EventId eventId = new EventId(new Guid());
+        throw new NotImplementedException();
     }
 }
