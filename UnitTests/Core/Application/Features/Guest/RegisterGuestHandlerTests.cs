d1d657ee6868d27b257c2db3e57f5066974941a3
Tomasz Grzesiak <grzesiaktomasz@gmail.com>
2025-09-24 15:24:21 +0200

    quick fixes

--- UnitTests/Core/Application/Features/Guest/RegisterGuestHandlerTests.cs
+++ UnitTests/Core/Application/Features/Guest/RegisterGuestHandlerTests.cs
@@ -2,6 +2,7 @@
 using ViaEventAssociation.Core.Application.AppEntry.Commands.Guest;
 using ViaEventAssociation.Core.Application.Features.Guest;
 using ViaEventAssociation.Core.Domain.Aggregates.Guests;
+using ViaEventAssociation.Core.Tools.OperationResult;
 
 namespace UnitTests.Core.Application.Features.Guest;
 
@@ -45,15 +46,13 @@ public class RegisterGuestHandlerTests
     public async Task HandleAsync_RegistersGuest_Fails_When_Email_Taken()
     {
         // Arrange
-
-
         var guestRepo = new InMemGuestRepoStub();
         var uow = new FakeUoW();
-        var checker = new FakeUniqueEmailChecker(["TAKN@via.dk"]);
+        var checker = new FakeUniqueEmailChecker(["takn@via.dk"]);
         var handler = new RegisterGuestHandler(guestRepo, uow, checker);
 
         var guestId = GuestId.CreateUnique();
-        const string emailAddress = "308817@via.dk";
+        const string emailAddress = "takn@via.dk";
         const string firstName = "Tomasz";
         const string lastName = "Grzesiak";
         const string pictureUrl = "https://example.com/pic.jpg";
@@ -64,16 +63,7 @@ public class RegisterGuestHandlerTests
         var result = await handler.HandleAsync(cmd);
 
         // Assert
-        Assert.True(result.IsSuccess);
-
-        var guest = await guestRepo.GetAsync(guestId);
-
-        Assert.NotNull(guest);
-        Assert.Equal(guest.Id, guestId);
-
-        Assert.Equal(pictureUrl, guest.ProfilePictureUrlAddress.ToString());
-        Assert.Equal(firstName, guest.FirstName.ToString());
-        Assert.Equal(lastName, guest.LastName.ToString());
-        Assert.Equal(emailAddress, guest.Email.ToString());
+        Assert.True(result.IsFailure);
+        Assert.Contains(Error.EmailAlreadyRegistered, result.Errors);
     }
 }
--- UnitTests/Fakes/FakeUniqueEmailChecker.cs
+++ UnitTests/Fakes/FakeUniqueEmailChecker.cs
@@ -3,11 +3,13 @@ using ViaEventAssociation.Core.Domain.Contracts;
 
 namespace UnitTests.Fakes;
 
-public class FakeUniqueEmailChecker(List<string> takenEmails) : IEmailUnusedChecker
+public class FakeUniqueEmailChecker(IEnumerable<string> takenEmails) : IEmailUnusedChecker
 {
+    private readonly HashSet<string> _takenEmails = new(takenEmails, StringComparer.OrdinalIgnoreCase);
+
     public Task<bool> IsUniqueAsync(EmailAddress emailAddress)
     {
-        var isUnique = !takenEmails.Any(t => t.Equals(emailAddress.ToString()));
+        var isUnique = !_takenEmails.Contains(emailAddress.ToString());
         return Task.FromResult(isUnique);
     }
 }
